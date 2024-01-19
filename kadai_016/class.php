bba233323b29e2ba6a5a34b89d09b07cfe305293
T2K2M <151506870+T2K2M@users.noreply.github.com>
2024-01-19 16:58:35 +0900

    Add files via upload

---
+++ kadai_016/class.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP課題</title>
+</head>
+<body>
+    <p>
+        <?php
+        // クラスを作成
+        class Food {
+            // プロパティを定義
+            public $name;
+            public $price;
+            // メソッドを定義
+            public function show_price(int $price) {
+                $this->price = $price;
+                echo $this->price . '<br>';
+            }
+            // コンストラクタを定義
+            public function __construct(string $name, int $price) {
+                $this->name = $name;
+                $this->price = $price;
+            }
+        }
+        // インスタンス化する
+        $food = new Food('potato' ,250);
+        print_r($food);
+        echo '<br>';
+
+        // クラスを作成する
+        class Animal {
+            // プロパティを定義
+            public $name;
+            public $height;
+            public $weight;
+            // メソッドを定義
+            public function show_height(int $height) {
+                $this->height = $height;
+                echo $this->height . '<br>';
+            }
+            // コンストラクタを定義
+            public function __construct(string $name, int $height, string $weight) {
+                $this->name = $name;
+                $this->height = $height;
+                $this->weight = $weight;
+            }
+        }
+        // インスタンス化
+        $animal = new Animal('dog' ,60, 5000);
+        print_r($animal);
+        echo '<br>';
+        
+        // メソッドへアクセス
+        $food->show_price(250);
+        $animal->show_height(60);
+        ?>
+    </p>
+</body>
+</html>
