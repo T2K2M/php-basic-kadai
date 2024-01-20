99af5ec87e799c004f0936f4a2220f7a085ef5ff
T2K2M <151506870+T2K2M@users.noreply.github.com>
2024-01-20 21:26:39 +0900

    Add files via upload

--- kadai_016/class.php
+++ kadai_016/class.php
@@ -7,53 +7,57 @@
 <body>
     <p>
         <?php
-         // クラスを作成する
-         class Food {
-            // プロパティを定義する
-            private $name;
-            private $price;
-            // メソッドを定義する
-            public function show_price() {
-                $this->price = 250;
-                echo $this->price . '<br>';
-            }
-            // コンストラクタを定義する
-            public function __construct(string $name, int $price) {
-                $this->name = $name;
-                $this->price = $price;
-            }
-        }
-        // インスタンス化する
-        $food = new Food('potato' ,250);
-        print_r($food);
-        echo '<br>';
-
-        // クラスを作成する
-        class Animal {
-            // プロパティを定義する
-            private $name;
-            private $height;
-            private $weight;
-            // メソッドを定義する
-            public function show_height() {
-                $this->height = 60;
-                echo $this->height . '<br>';
-            }
-            // コンストラクタを定義する
-            public function __construct(string $name, int $height, string $weight) {
-                $this->name = $name;
-                $this->height = $height;
-                $this->weight = $weight;
-            }
-        }
-        // インスタンス化する
-        $animal = new Animal('dog' ,60, 5000);
-        print_r($animal);
-        echo '<br>';
-        
-        // メソッドへアクセスする
-        $food->show_price();
-        $animal->show_height();
+         class Food
+         {
+             private $name;
+             private $price;
+         
+             public function show_price($price)
+             {
+                 echo $this->price;
+             }
+         
+             public function __construct($name, $price)
+             {
+                 $this->name = $name;
+                 $this->price = $price;
+             }
+         }
+         
+         $food = new Food('potato', 250);
+         print_r($food);
+         echo '<br>';
+         
+         class Animal
+         {
+             private $name;
+             private $height;
+             private $weight;
+         
+             public function show_height($height)
+             {
+                 echo $this->height;
+         
+             }
+         
+             public function __construct($name, $height, $weight)
+             {
+                 $this->name = $name;
+                 $this->height = $height;
+                 $this->weight = $weight;
+             }
+         }
+         
+         
+         $animal = new Animal('dog', 60, 5000);
+         print_r($animal);
+         echo '<br>';
+         
+         $food->show_price(250);
+         echo '<br>';
+         
+         $animal->show_height(60);
+         
         ?>
      
         
